ee8ecc56f91c582d8ce330fcde1f2a1166d515cc
Bill <207559339@qq.com>
2019-09-30 09:32:28 +0800

    adding comments

--- src/excercise/anagram.js
+++ src/excercise/anagram.js
@@ -6,33 +6,34 @@ var anagram = function(x){
     } else{
         console.log("please return a string")
         return false
-    }
+    } // controlling the flow
+      // my logic is that if it's not a string I won't bother to assign it to the variable I will use below
 
-    var lowerCaseString1 = string1.toLowerCase()
-    var lowerCaseArray1 = lowerCaseString1.split("")
-    // console.log(a1)
-    // var la2 = []
+    var lowerCaseString1 = string1.toLowerCase() // making it not case-sensitive
+    var lowerCaseArray1 = lowerCaseString1.split("") // plit the lowercase string in to an array
+    // console.log(a1) // checking while coding
+    // var la2 = [] 
     // for(ii=0; ii<ls1.length; ii++){
     //     la2.unshift(la1[ii])
-    // }
+    // } // I need to check methods more, I litterally build a reverse method
     // // console.log(a2)
-    var lowerCaseArray2 = lowerCaseArray1.reverse()
-    var lowerCaseString2 = lowerCaseArray2.join("")
-    // console.log(ls2)
+    var lowerCaseArray2 = lowerCaseArray1.reverse() // the most efficient way of reversing an array
+    var lowerCaseString2 = lowerCaseArray2.join("") // put the array back into a string
+    // console.log(ls2) // checking
     
-    return lowerCaseString2 === lowerCaseString1
+    return lowerCaseString2 === lowerCaseString1 
     // if(lowerCaseString2 === lowerCaseString1){
     //     return true
     // } else {
     //     return false
-    // }
+    // } // not a very code saving way to do this
 }
 
-console.log(anagram(1))
+console.log(anagram(1)) // checking
 
 
 
-// var isAnagram2 = function(word){
-//     return word.toLowerCase() === word.toLowerCase().split('').reverse().join('')
-// }
-// console.log(isAnagram2('Level'))
+var isAnagram2 = function(word){
+    return word.toLowerCase() === word.toLowerCase().split('').reverse().join('') // needs to be very familiar with the methods
+}
+console.log(isAnagram2('Level'))
--- src/excercise/authentication.js
+++ src/excercise/authentication.js
@@ -1,52 +1,52 @@
-// const userName = "Bill"
-// const passWord = "helloWorld"
+const userName = "Bill" 
+const passWord = "helloWorld" //defining the credentials
 
-// const authentication = function(name, password){
-//     if(typeof name !== "string" || typeof password !== "string"){
-//         console.log("please type in a string for user name and pass word")
-//         return false
-//     }
-//     if(userName.toLowerCase()===name.toLowerCase()){
-//         if(password===passWord){
-//             return true
-//         } else {
-//             return "please enter the correct user name and password!"
-//         }
-//     } 
-// }
+const authentication = function(name, password){ 
+    if(typeof name !== "string" || typeof password !== "string"){ // if either of the two imputs are not string
+        console.log("please type in a string for user name and pass word") // give some hints to users
+        return false
+    } // handle errors; flow control
+    if(userName.toLowerCase()===name.toLowerCase()){ // "name" doesn't have to be case-sensitive
+        if(password===passWord){ // "password" needs to match with credentials exactly
+            return true
+        } else { // don't forget the else or will return undefined if imputs ain't correctt
+            return "please enter the correct user name and password!"
+        }
+    } 
+} //setting up the authentication function
 
-// console.log(authentication("BIll","HelloWorld"))
+console.log(authentication("BIll","HelloWorld")) // checking
 
 
 var credentials = {
     username: "Bill",
     password: "Zhang"
-}
+} // setting defaults with objects
 
 const compare =  function(compareFrom, compareTo, caseSensitive){
-    let v1, v2
+    let v1, v2 // because the v1, v2 should be changeable, we use "let"
     
-    if(caseSensitive){
-        v1=compareFrom
+    if(caseSensitive){ //case-sensitive
+        v1=compareFrom // reassign it's value
         v2=compareTo
-    } else {
+    } else { //not case-sensitive
         v1 = compareFrom.toLowerCase()
         v2 = compareTo.toLowerCase()
     }
 
-    return v1 === v2
-}
+    return v1 === v2 // writing a if ... else ... statement is basically dealing with booleans, therefor it is more sensible to do this
+} // reusable piece of code can be turned into a function
 
 const login = function(username, password){
     if (typeof unsername !== "string" || typeof password !== "string" ){
         console.log('please enter a user name and a password')
         return false
-    }
+    } // handling errors at the top
 
-    const checkUsername = compare(username,credentials.username)
-    const checkPassword = compare(password,credentials.password, true)
+    const checkUsername = compare(username,credentials.username) // false has been omitted
+    const checkPassword = compare(password,credentials.password, true) // using the function
 
-    return checkUsername && checkPassword
+    return checkUsername && checkPassword // both checkUsername and checkPassword are booleans, if both true return true
 }
 
 console.log(login('bill',"Zhang")) // true
